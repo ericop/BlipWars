bde5defde0da10c8c522d22d981321739d18e4fe
ericop <eric.op.johnson@gmail.com>
2017-04-11 00:47:05 -0400

    trying to get absolute x,y for center of playerChoiceArea to compare with click, but its giving some scaled number

--- Assets/scripts/PlayerChoiceBoxController.cs
+++ Assets/scripts/PlayerChoiceBoxController.cs
@@ -5,47 +5,56 @@ using UnityEngine;
 public class PlayerChoiceBoxController : MonoBehaviour
 {
 
-    public Renderer rend;
+    float xOfClick = 0;
+    float yOfClick = 0;
+
+    float xCenter = 0;
+    float yCenter = 0;
+
     void Start()
     {
-        rend = GetComponent<Renderer>();
+        var shape = GetComponent<Collider2D>();
+        var selectRectangleCenter = shape.bounds.center;
+
+        Vector3 pos = Camera.main.ScreenToWorldPoint(transform.position);
+
+        xCenter = transform.position.x;
+        yCenter = transform.position.y;
+        Debug.Log("centers x:" + xCenter + ", y:" + yCenter);
+
     }
-    void OnMouseEnter()
+
+    void OnMouseDown()
     {
-        rend.material.color = Color.red;
-    }
-    void OnMouseOver()
-    {
-        rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
-    }
-    void OnMouseExit()
-    {
-        rend.material.color = Color.white;
+        xOfClick = Input.mousePosition.x;
+        yOfClick = Input.mousePosition.y;
+        Debug.Log("clicks x:" + xOfClick + ", y:" + yOfClick);
+        Debug.Log("x:" + (xOfClick - xCenter) + ", y:" + (yOfClick - yCenter));
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // if (Input.GetMouseButtonDown(0) 
-        //     //&&
-        //     //Input.mousePosition.x > 648 &&
-        //     // Input.mousePosition.x < 1048 && 
-        //     // Input.mousePosition.y > 21 && 
-        //     // Input.mousePosition.y < 190 
-        //      )
-        // {
-        //     //var mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        //     //Debug.Log("mousePosition:" + mousePosition);
-        // }
-        //if (Input.touchCount > 0 && Input.touchCount < 3)
-        // {
-        //     var touchPosition = Input.GetTouch(1).position;
-        //     Debug.Log("touchPosition1:" + touchPosition);
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0)
+    //         //&&
+    //         //Input.mousePosition.x > 648 &&
+    //         // Input.mousePosition.x < 1048 && 
+    //         // Input.mousePosition.y > 21 && 
+    //         // Input.mousePosition.y < 190 
+    //         )
+    //    {
+    //        //var mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    //        //Debug.Log("mousePosition:" + mousePosition);
+    //    }
+    //    if (Input.touchCount > 0 && Input.touchCount < 3)
+    //    {
+    //        var touchPosition = Input.GetTouch(1).position;
+    //        Debug.Log("touchPosition1:" + touchPosition);
 
-        //     var touchPosition2 = Input.GetTouch(2).position;
-        //     Debug.Log("touchPosition2:" + touchPosition2);
+    //        var touchPosition2 = Input.GetTouch(2).position;
+    //        Debug.Log("touchPosition2:" + touchPosition2);
 
-        // }
+    //    }
 
-    }
+    //}
 }
