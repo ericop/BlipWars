9a4af3c40200ed4c17eb5db234578e179eea7c92
ericop <eric.op.johnson@gmail.com>
2017-04-15 19:16:11 -0400

    try apk build again

--- Assets/scripts/SaveEnergyController.cs
+++ Assets/scripts/SaveEnergyController.cs
@@ -7,12 +7,10 @@ public class SaveEnergyController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     void OnMouseDown()
